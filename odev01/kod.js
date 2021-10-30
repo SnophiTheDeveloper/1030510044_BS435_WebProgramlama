5eec893eda1f918b2bcc515bcc4a5e28ef6d7e41
Ayberk <ayberkugurlu@gmail.com>
2021-10-30 16:11:31 +0300

    Odev01_Son

---
+++ odev01/kod.js
@@ -0,0 +1,29 @@
+const imgs =[
+    cat="./img/cat.jpg",
+    dog="./img/dog.jpg"
+];
+
+var flag = 0;
+
+function findCat(imgid) {
+    let index = Math.floor(Math.random()*2);
+
+    document.getElementById(imgid).src=imgs[index];
+    document.getElementById(imgid).onclick=null;
+    if(document.getElementById(imgid).src.match(imgs[0]))
+    {   
+        document.getElementById("alanId").innerHTML="Tebrikler! Oyunu kazandınız. Tekrar oyun oynamak için <span class='link' onclick=\"window.location.reload();\">buraya</span> tıklayabilirsiniz";
+        for(var i=0; i<3;i++)
+        {document.getElementById("img"+i).onclick=null;}
+    }
+    else
+    {
+        flag++;
+        if(flag==2)
+        {
+            document.getElementById("alanId").innerHTML="Üzgünüm! Oyunu kaybettiniz. Tekrar oyun oynamak için <span class='link' onclick=\"window.location.reload();\">buraya</span> tıklayabilirsiniz";
+            for(var i=0; i<3;i++)
+            {document.getElementById("img"+i).onclick=null;}
+        }
+    }
+}
